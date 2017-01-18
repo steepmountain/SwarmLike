a277a51384971b72f35f2a0c035b314e389d579d
Steepmountain <rubenbratsberg@gmail.com>
2017-01-18 20:33:46 +0100

    Master
    Master

---
+++ Assets/Scripts/BackgroundScroll.cs
@@ -0,0 +1,23 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class BackgroundScroll : MonoBehaviour {
+
+    public float speed;
+
+	void Update () {
+
+        MeshRenderer mr = GetComponent<MeshRenderer>();
+
+        Material mat = mr.material;
+
+        Vector2 offset = mat.mainTextureOffset;
+
+        offset.x = transform.position.x;
+        offset.y = transform.position.y;
+
+        mat.mainTextureOffset = offset / speed;
+
+	}
+}
